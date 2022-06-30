ac97eb9dc5460d12f56f1882f00ca1fcb8e28bef
Xiang ZHENG <xiangzheng2021@outlook.com>
2022-06-29 21:16:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -58,4 +58,19 @@ def main():
                   header=["Class", "Times of tutorial", "Followers per time", "Spells per time","Cards per time","Evos per time"])
         put_button("Back", onclick=lambda: main())
         
-main()
+if __name__ == '__main__':
+    import argparse
+    from pywebio.platform.tornado_http import start_server as start_http_server
+    from pywebio import start_server as start_ws_server
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
+    args = parser.parse_args()
+
+    if args.http:
+        start_http_server(main, port=args.port)
+    else:
+        # Since some cloud server may close idle connections (such as heroku),
+        # use `websocket_ping_interval` to  keep the connection alive
+        start_ws_server(main, port=args.port, websocket_ping_interval=30)
